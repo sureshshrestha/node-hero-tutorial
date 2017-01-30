ce07cd432b7bd9402c6db99998bd21b72843a6fc
Suresh Shrestha <surestha@outlook.com>
2017-01-30 14:31:17 +0545

    3. Async programming in node js complete.

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const calc = require('./calc')
 const _ = require('lodash')
+const fs = require('fs')
 
 _.assign({ 'a': 1}, {'b': 2}, {'c': 3});
 
@@ -12,3 +13,23 @@ const numbersToAdd = [
 
 const result = calc.sum(numbersToAdd)
 console.log(`This result is: ${result}`)
+
+function stats (file) {
+    return new Promise((resolve, reject) => {
+	fs.stat(file,(err, data) => {
+	    if (err) {
+		return reject (err)
+            }
+	    resolve(data)
+    })
+})
+
+}
+
+Promise.all([
+  stats('file1'),
+  stats('file2'),
+  stats('file3')
+])
+.then((data) => console.log(data))
+.catch((err) => console.log(err))
